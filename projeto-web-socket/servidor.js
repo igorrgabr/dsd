11be2e0038a5f007632277d9ed0e64eb2cba4dd0
KdtVinicius <vinispencer@outlook.com>
2024-01-03 19:14:46 -0300

    fazendo projeto

--- projeto-web-socket/servidor.js
+++ projeto-web-socket/servidor.js
@@ -2,40 +2,41 @@ const WebSocket = require('ws');
 const http = require('http');
 const fs = require('fs');
 
-// Criar um servidor HTTP para lidar com solicitações iniciais
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('WebSocket server is running.');
 });
 
-// Criar um servidor WebSocket associado ao servidor HTTP
 const wss = new WebSocket.Server({ server });
 
-// Evento de conexão com o cliente WebSocket
 wss.on('connection', (ws) => {
   console.log('Cliente conectado.');
 
-  // Evento de recebimento de mensagem do cliente
   ws.on('message', (message) => {
     console.log('Mensagem recebida:', message);
 
-    // Se a mensagem for um caminho de arquivo, transmita o arquivo
     if (fs.existsSync(message)) {
       const fileStream = fs.createReadStream(message);
-      fileStream.pipe(ws);
+      fileStream.on('data', (chunk) => {
+        // Envie os dados do arquivo como um Buffer
+        ws.send(chunk);
+      });
+
+      fileStream.on('end', () => {
+        // Feche a conexão após o envio completo do arquivo
+        ws.send('Fechando conexão. Arquivo enviado com sucesso.');
+        ws.close();
+      });
     } else {
-      // Se não for um caminho de arquivo válido, envie uma mensagem de erro
       ws.send('Arquivo não encontrado.');
     }
   });
 
-  // Evento de fechamento da conexão com o cliente
   ws.on('close', () => {
     console.log('Cliente desconectado.');
   });
 });
 
-// Inicie o servidor HTTP
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Servidor HTTP e WebSocket ouvindo na porta ${PORT}`);
